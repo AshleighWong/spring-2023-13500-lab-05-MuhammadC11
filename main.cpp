e21c11359478ff8d543fb76562219c31c2f7468f
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-02-26 14:00:22 -0500

    trying to figure out tests, getting compiler errors

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-
 #include "funcs.h"
 
 int main()
 {
+  std::cout << isDivisibleBy(100, 25) << std::endl;
+  std::cout << isDivisibleBy(100, 24) << std::endl;
+  std::cout << isDivisibleBy(100, 100) << std::endl;
+  std::cout << isDivisibleBy(100, 1) << std::endl;
+  std::cout << isDivisibleBy(100, 0) << std::endl;
   return 0;
 }
--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,12 @@
 #include "funcs.h"
 
 // add your tests here
+
+TEST_CASE("Testing isDivisibleBy")
+{
+    CHECK(isDivisibleBy(100, 25) == true);
+    CHECK(isDivisibleBy(100, 24) == false);
+    CHECK(isDivisibleBy(100, 100) == true);
+    CHECK(isDivisibleBy(100, 1) == true);
+    CHECK(isDivisibleBy(100, 0) == false);
+}
